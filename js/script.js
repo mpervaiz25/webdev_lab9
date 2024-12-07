c79b505b370e31e3126f952c7b855e3f31692af0
mpervaiz25 <mpervaiz@hawk.iit.edu>
2024-12-07 15:21:04 -0600

    function to click button and get inputted text

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 function init(){
 //add your javascrip between these two lines of code
+  var button = document.getElementById('entrybutton');// Find the button with the ID 'entrybutton' in the HTML file code
  
+ function showMeText() {// A Function to execute when the button is clicked on
+	 var textbox = document.getElementById('entryinput');// Gets the inputted text with the ID 'entryinput'
 
 
 
