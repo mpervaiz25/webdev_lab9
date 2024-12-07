950d714e78da4ca1ff97156cb7dde7e34ba9b94f
mpervaiz25 <mpervaiz@hawk.iit.edu>
2024-12-07 15:39:44 -0600

    updating the h2 content

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ function init(){
     var textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
     alert("Mashell Pervaiz: " + textbox.value);
+		
+		var h2Element = document.querySelector('h2');
+		h2Element.textContent = textbox.value; // Updates the <h2> content
+		
   }
 
   button.addEventListener('click', showMeText);
